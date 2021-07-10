8cbe3aa81911f3b922487fc977647b7c7fdfa4f4
air7air <aching_e@hanmail.com>
2021-07-11 03:48:56 +0900

    add

--- 09_Shotest_Path/5.py
+++ 09_Shotest_Path/5.py
@@ -2,7 +2,7 @@ import heapq
 import sys
 input = sys.stdin.readline
 INF = int(1e9) # 무한을 의미하는 값으로 10억을 설정
-###
+
 # 노드의 개수, 간선의 개수, 시작 노드를 입력받기
 # n, m, start = map(int, input().split())
 n, m, start = 3, 2, 1
