0539d1803bd47bf115187f880c34fb58bbc4a1ae
air7air <aching_e@hanmail.net>
2021-07-17 04:04:13 +0900

    add

--- 04_implementation/04_323.py
+++ 04_implementation/04_323.py
@@ -1,24 +1,21 @@
-input1="abcabca"
+input1='abcabcdede'
 index1=[0]*len(input1)
-
-# for i in range(1,len(input1)):
-#     print("i",i)
-    #2일때
-
-answer=0
-i=3
-quo=len(input1)//i
-remainder=len(input1)//i
-final=[1]*quo
-for j in range(quo):
-    if input1[j*i:((j+1)*3)-1]==input1[(j+1)*i:((j+2)*3)-1]: #input1[0:2] != input1[3:5]:   [6:8]
-        final[j]=0
-print(i*(i+1))
-##1로 된것을 더해야 함
+input1=[i for i in input1]
+how=len(input1)//2
+final=len(input1)
+for n in range(2,how):
+    ii = []
+    for i in range(0,len(input1)-len(input1)%n,n): #n개씩 나누기
+        ii.append(input1[i:i+n])
+    print(ii)
+    array1=[0]*len(ii)#n개 0으로 된 배열 만들기
+    for i in range(len(ii)-1):
+        if ii[i]==ii[i+1]: #앞뒤 같으면1
+            array1[i]=1
+    answer=0
+    for i in range(len(array1)):
+        if array1[i] == 1 and array1[i+1]==0 : ## 1,0 ->sum
+            answer+=1
+    print("답:",answer+len(input1)%n+array1.count(0)*n) #1,0인것 카운트+나머지+0인거 카운트*n
+    final=min(final,answer+len(input1)%n+array1.count(0)*n)
 print(final)
-
-
-
-        #0:2    3:5
-
-
